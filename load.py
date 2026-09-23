c44b39ad9518989aee5d8514e819dcf35db4984c
Rohit-Roby <rohitroby12@gmail.com>
2026-09-23 18:34:06 +0100

    Add GCP storage and Big Query pipeline integration

--- load.py
+++ load.py
@@ -1,5 +1,6 @@
 import json
 import csv
+from google.cloud import storage, bigquery
 import logging
 logger = logging.getLogger(__name__)
 
@@ -20,3 +21,52 @@ def save_csv(data, filename):
         writer.writeheader()
         writer.writerows(data)
     logger.info('Data successfully saved %d records to %s', len(data), filename)
+
+def upload_to_gcs(local_file, bucket_name, destination_blob):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob)
+    blob.upload_from_filename(local_file)
+
+    logger.info(
+        "Uploaded %s to gs://%s/%s",
+        local_file,
+        bucket_name,
+        destination_blob
+    ) 
+
+
+def load_gcs_csv_to_bigquery(bucket_id, blob_name, project_id, dataset_id, table_id, location):
+    client = bigquery.Client(project = project_id)
+    source_uri = f"gs://{bucket_id}/{blob_name}"
+    destination_table =(f"{project_id}.{dataset_id}.{table_id}")
+
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+             bigquery.SchemaField("id", "INTEGER"),
+            bigquery.SchemaField("category", "STRING"),
+            bigquery.SchemaField("latitude", "FLOAT"),
+            bigquery.SchemaField("longitude", "FLOAT"),
+            bigquery.SchemaField("street", "STRING"),
+            bigquery.SchemaField("month", "STRING")
+        ], source_format = bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        write_disposition=(
+            bigquery.WriteDisposition.WRITE_TRUNCATE
+        )
+    )
+
+    load_job = client.load_table_from_uri(
+        source_uri,
+        destination_table,
+        job_config = job_config,
+        location = location
+    )
+
+    load_job.result()
+
+    table = client.get_table(
+        destination_table
+    )
+
+    logger.info("Loaded %d rows into BigQuery table %s", table.num_rows, destination_table)
--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import logging
 from dotenv import load_dotenv
 from extract import fetch_crime_data
 from transform import clean_records
-from load import save_json, save_csv
+from load import save_json, save_csv, upload_to_gcs, load_gcs_csv_to_bigquery
 from pathlib import Path
-
 LOG_DIR = Path("logs")
 LOG_DIR.mkdir(exist_ok=True)  # Create logs directory if it doesn't exist
 
@@ -35,6 +34,12 @@ def main():
     logger.info("pipeline started")
 
     url = os.getenv('POLICE_API_URL')
+    project_id = os.getenv("GCP_PROJECT_ID")
+    region = os.getenv("GCP_REGION")
+    dataset_id = os.getenv("BIGQUERY_DATASET")
+    table_id = os.getenv("BIGQUERY_TABLE")
+
+
     params = {
         'date': os.getenv('CRIME_DATE'),
         'lat': float(os.getenv('LATITUDE')),
@@ -58,6 +63,26 @@ def main():
 
     save_json(cleaned_data, jsonfilename)
     save_csv(cleaned_data, csvfilename)
+
+    # LOADING CLEANED DATA TO GCP BUCKET
+    bucket_name = os.getenv('GCS_BUCKET_NAME')
+    crime_date = os.getenv('CRIME_DATE')
+    json_blob = f"cleaned/{crime_date}/cleaned_crime_data.json"
+    csv_blob = f"cleaned/{crime_date}/cleaned_crime_data.csv"
+
+    upload_to_gcs(jsonfilename, bucket_name, json_blob)
+    upload_to_gcs(csvfilename, bucket_name, csv_blob)
+
+
+    load_gcs_csv_to_bigquery(
+        bucket_id=bucket_name,
+        blob_name=csv_blob,
+        project_id=project_id,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        location=region
+        )
+
     logger.info('Pipeline completed successfully. Cleaned data saved at %s and %s ', jsonfilename, csvfilename)
 if __name__ == "__main__":
     main()
